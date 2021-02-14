e834815f88d1741e332401e5d9ac47ebe4e44fba
mcflav <nc2jax@hotmail.com>
2021-02-14 04:10:09 -0500

    refactor coded by adding service class

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../shared/order.model';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  styleUrls: ['./order-list.component.css'],
+  providers: [OrderService]
 })
 export class OrderListComponent implements OnInit {
-  total;
-
-  orders: Order[] = [
-    new Order('columbia', 'caf', 'small', 1, 2.55, 1 * 2.55),
-    new Order('french roast', 'decaf', 'medium', 2, 3.55, 2 * 3.55),
-    new Order('breakfast blend', 'caf', 'large', 3, 4.55, 3 * 4.55)
-  ];
-  constructor() { }
+  orders: Order[];
+  // orders: Order[] = [
+  //   new Order('columbia', 'caf', 'small', 1, 2.55, 1 * 2.55),
+  //   new Order('french roast', 'decaf', 'medium', 2, 3.55, 2 * 3.55),
+  //   new Order('breakfast blend', 'caf', 'large', 3, 4.55, 3 * 4.55)
+  // ];
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
+      this.orders = this.orderService.getOrders();
+      this.orderService.orderChanged
+        .subscribe(
+            (orders: Order[]) => {
+                this.orders = orders;
+            }
+        );
+
   }
 
 
-  addedOrder(order: Order){
-    this.orders.push(order);
-  }
+  //addedOrder(order: Order){
+  //   this.orders.push(order);
+  // }
 
 }
