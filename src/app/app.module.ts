ae113e877d62c6c628bbb27fea08ebf484990c5a
mcflav <nc2jax@hotmail.com>
2021-08-26 22:13:31 -0400

    updated feature Modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { ShowHidePasswordModule } from 'ngx-show-hide-password';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,31 +7,31 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
-import { OrderItemModule } from './order-items/order-items.module';
+
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+//import { FormsModule } from '@angular/forms';
+import { OrderItemModule } from './order-items/order-items.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    //FormsModule,
     ShowHidePasswordModule,
     AppRoutingModule,
     HttpClientModule,
     RegisterModule,
     LoginModule,
-    OrderItemModule,
     CoreModule,
-    AboutComponent,
-    SharedModule
+    SharedModule,
+    OrderItemModule
   ],
   bootstrap: [AppComponent]
 })
