3ffd6bfc9117426403d80269e08ade376d706fa9
mcflav <nc2jax@hotmail.com>
2021-08-26 22:46:15 -0400

    added lazy loading code to routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,9 @@ import { ShowHidePasswordModule } from 'ngx-show-hide-password';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RegisterModule } from './register/register.module';
-import { LoginModule } from './login/login.module';
-
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
-//import { FormsModule } from '@angular/forms';
 import { OrderItemModule } from './order-items/order-items.module';
 
 
@@ -23,15 +19,11 @@ import { OrderItemModule } from './order-items/order-items.module';
   ],
   imports: [
     BrowserModule,
-    //FormsModule,
     ShowHidePasswordModule,
     AppRoutingModule,
     HttpClientModule,
-    RegisterModule,
-    LoginModule,
     CoreModule,
     SharedModule,
-    OrderItemModule
   ],
   bootstrap: [AppComponent]
 })
